87658401bf613aa4459e3a66dbb6200ca42cc089
Brian Leathem <bleathem@gmail.com>
2015-06-05 20:39:52 -0700

    Removed the hexboard code from the beacon project

--- server/server.js
+++ server/server.js
@@ -22,8 +22,6 @@ var dataInit = Rx.Observable.forkJoin(
   require('./beacon/ws/beacon-playback')(server);
   require('./beacon/ws/beacon-random')(server);
   require('./beacon/ws/broker')(server);
-  require('./thousand/ws/thousand')(server);
-  require('./thousand/ws/winner')(server);
 })
 .subscribeOnError(function(err) {
   console.log(err.stack || err);
--- server/thousand/thousandEmitter.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-
-var EventEmitter = require("events").EventEmitter
- ,  thousandEmitter = new EventEmitter();
-
- module.exports = thousandEmitter;
