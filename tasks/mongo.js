bf1e9fb30045d49b6fe16b0a7ef6b3e3723f8957
Brian Leathem <bleathem@gmail.com>
2015-06-09 08:50:23 -0700

    Create the data dir when starting mongo

--- tasks/mongo.js
+++ tasks/mongo.js
@@ -1,10 +1,18 @@
 'use strict';
 
 var exec = require('child_process').exec
+  , fs = require('fs')
   ;
 
 module.exports = function(gulp, opts) {
   gulp.task('mongo', function (done) {
+    try {
+      fs.mkdirSync('data');
+    } catch (e) {
+      if (e.code !== 'EEXIST') {
+        throw e;
+      }
+    }
     console.log(opts.paths.data);
     exec('mongod --dbpath ' + opts.paths.data, function (error, stdout, stderr) {
       console.log('stdout: ' + stdout);
