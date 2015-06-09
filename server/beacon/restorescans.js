238874234d02455ae2e005e6a2c48e5485166b7a
Brian Leathem <bleathem@gmail.com>
2015-06-09 12:42:10 -0700

    Added some timing logs to the scan restore

--- server/beacon/restorescans.js
+++ server/beacon/restorescans.js
@@ -6,7 +6,12 @@ var Rx = require('rx')
   , getUser = require('../api/user/user').getUser
   ;
 
+var tag = 'RESTORE';
+
 var restoreScans = function() {
+  var start = new Date();
+  console.log(tag, start);
+  console.log(tag, 'Starting restore scan query');
   return Scan.aggregate()
     .sort({'timestamp': -1})
     .group({
@@ -23,6 +28,10 @@ var restoreScans = function() {
     })
     .exec()
     .then(function(scans) {
+      var end = new Date();
+      console.log(tag, end);
+      console.log(tag, 'Ending restore scan query');
+      console.log(tag, (end.getTime() - start.getTime())/1000, 'seconds elapsed.');
       return scans.map(function(scan) {
         scan.user = getUser(scan.beaconId);
         scan.location = convertLocation(scan.locationCode);
