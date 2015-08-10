6872a64aaaa1738d8515f3094da56cc84189e289
Brian Leathem <bleathem@gmail.com>
2015-08-10 13:55:58 -0700

    Used npm for client-side deps instead of bower

--- server/main/config.js
+++ server/main/config.js
@@ -14,6 +14,7 @@ module.exports = exports = function (app, express, routers) {
   app.use(middle.cors);
   var staticRoot = __dirname + '/../../client/src';
   app.use(express.static(__dirname + '/../../client'));
+  app.use('/node_modules', express.static(__dirname + '/../../node_modules'));
   app.use(morgan('dev'));
   app.use('/api', routers.apiRouter);
   app.use(middle.logError);
